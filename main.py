d0c4143869a6d89c378af4cb5e57948d26ebe7ad
Pietro <pietroepis@gmail.com>
2020-11-17 19:17:38 +0100

    DS import, report write

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
 import re
+from violations_ds import violations
 
 """
         0        1         2        3      4      5       6        7         8           9
     <seqname> <source> <feature> <start> <end> <score> <strand> <frame> [attributes] [comments] 
 """
 
+def write_report():
+    n_issues = 0
+    file_out = open("report.txt", "w")
+    file_out.write("#\tRecord\tProblem\n")
+
+    if violations["source_not_unique"]:
+        n_issues += 1
+        file_out.write(str(n_issues) + "\t" + "/" + "\t\t" + "\"source\" field must be unique\n")
+
+    if violations["missing_cds"]:
+        n_issues += 1
+        file_out.write(str(n_issues) + "\t" + "/" + "\t\t" + "A \"CDS\" record is required\n")
+
+    if violations["missing_start_codon"]:
+        n_issues += 1
+        file_out.write(str(n_issues) + "\t" + "/" + "\t\t" + "A \"start_codon\" record is required\n")
+
+    if violations["missing_stop_codon"]:
+        n_issues += 1
+        file_out.write(str(n_issues) + "\t" + "/" + "\t\t" + "A \"stop_codon\" record is required\n")
+
+    if violations["start_codon_more_than_3"]:
+        n_issues += 1
+        file_out.write(str(n_issues) + "\t" + "/" + "\t\t" + "start_codon total lenght must be less than or equal to 3\n")
+
+    if violations["stop_codon_more_than_3"]:
+        n_issues += 1
+        file_out.write(str(n_issues) + "\t" + "/" + "\t\t" + "stop_codon total lenght must be less than or equal to 3\n")
+        
+    
+
 def is_field_valid(field, allowed_values):
     return True if field in allowed_values else False
 
 def parse_attributes(field):
-    return re.findall(r"([\w\s]+) ([\w\"]+)", field)
+    return re.findall(r"(\s?\w+) ([^;]+;)", field)
+    #return re.findall(r"([\w\s]+) ([\w\"]+;?)", field)
     #return re.findall(r"([\w\s]+) (\"[^\"]+\"|[0-9]+)", field)
 
 file_name = "./samples/file-1.gtf"
+start_codon_bp, stop_codon_bp = 0, 0
 lines = []
-violations = {
-    "missing_cds": True,
-    "missing_start_codon": True,
-    "missing_stop_codon": True,
-    "start_codon_more_than_3": False,
-    "stop_codon_more_than_3": False,
-    "invalid_fields_number": [],
-    "start_codon_invalid_frame": [],
-    "stop_codon_invalid_frame": [],
-    "inter_trascript_id_not_empty": [],
-    "inter_CNS_trascript_id_not_empty": [],
-    "intron_CNS_trascript_id_empty": [],
-    "start_not_valid": [],
-    "stop_not_valid": [],
-    "start_greater_than_end": [],
-    "score_invalid": [],
-    "strand_invalid": [],
-    "frame_invalid": [],
-    "missing_gene_id": [],
-    "missing_transcript_id": [],
-    "invalid_attributes_order": [],
-    "invalid_attributes_separator": [],
-    "text_attribute_not_doublequotes": []
-}
 
 with open(file_name, "r") as input_file:
     lines = input_file.readlines()
@@ -54,8 +64,14 @@ for index in range(0, 20):
 
     # Record fields number check 
     if (len(record) < 9):
-        violations["invalid_fields_number"].append(index)
+        violations["invalid_fields_number"]["value"].append(index)
         continue
+
+    # source check
+    # 2nd field (index 1)
+    # source must be unique in the file
+    if index > 0 and record[1] != lines[index-1][1]:
+        violations["source_not_unique"]["value"] = True
 
     # feature check
     # 3rd field (index 2)
@@ -68,78 +84,97 @@ for index in range(0, 20):
     # CDS feature check
     # A "CDS" record is required
     if record[2] == "CDS":
-        violations["missing_cds"] = False
+        violations["missing_cds"]["value"] = False
 
     # start_codon feature check
     # A "start_codon" record is required
     if record[2] == "start_codon":
-        violations["missing_start_codon"] = False            
+        violations["missing_start_codon"]["value"] = False     
+        start_codon_bp += record[4] - record[3] + 1       
 
     # stop_codon feature check
     # A "stop_codon" record is required
     if record[2] == "stop_codon":
-        violations["missing_stop_codon"] = False
+        violations["missing_stop_codon"]["value"] = False
+        stop_codon_bp += record[4] - record[3] + 1   
             
-    # start and stop check
+    # start and end check
     # 4th field (index 3) and 5th field (index 4) respectively
     # start must be less than or equal to stop
     valid_start_stop = True
     if not (record[3].isdigit() and int(record[3]) >= 1):
-        violations["start_not_valid"].append(index)
+        violations["start_not_valid"]["value"].append(index)
         valid_start_stop = False
 
     if not (record[4].isdigit() and int(record[4]) >= 1):
-        violations["stop_not_valid"].append(index)
+        violations["end_not_valid"]["value"].append(index)
         valid_start_stop = False
 
     if (valid_start_stop and record[3] > record[4]):
-        violations["start_greater_than_end"].append(index)
+        violations["start_greater_than_end"]["value"].append(index)
 
     # score check
     # 6th field (index 5)
     # It must be an Integer or Floating Point value. "." allowed too
     if not (record[5] == "." or record[5].isnumeric()):
-        violations["score_invalid"].append(index)
+        violations["score_invalid"]["value"].append(index)
 
     # strand check
     # 7th field (index 6)
     # Allowed value: {+, -}
     if not is_field_valid(record[6], {"+", "-"}):
-        violations["strand_invalid"].append(index) 
+        violations["strand_invalid"]["value"].append(index) 
 
     # frame check
     # 8th field (index 7)
     # Generic record allowed values: {0, 1, 2, .}
     # "start_codon" or "stop_codon" feature allowed values: {0, 1, 2}
     if not is_field_valid(record[7], {"0", "1", "2", "."}):
-        violations["frame_invalid"].append(index)
+        violations["frame_invalid"]["value"].append(index)
         
     if record[2] == "start_codon" and not is_field_valid(record[7], {"0", "1", "2"}):
-        violations["start_codon_invalid_frame"].append(index)
+        violations["start_codon_invalid_frame"]["value"].append(index)
     elif record[2] == "stop_codon"and not is_field_valid(record[7], {"0", "1", "2"}):
-        violations["stop_codon_invalid_frame"].append(index)
+        violations["stop_codon_invalid_frame"]["value"].append(index)
 
     # attributes check
     # 9th field (index 8)
+    # Every record must have gene_id and transcript_id attributes
+    # Any other attribute must apper after these two
+    # Every attribute must end in a semicolon and be separated from the next one by exactly one space
     found_gene_id, found_transcript_id = False, False
     for j, attribute in enumerate(parse_attributes(record[8])):
-        if j > 0 and attribute[0][0] != " ":
-            violations["invalid_attributes_separator"].append(index)
+        if attribute[1][-1] != ";" or (j > 0 and not (attribute[0][0] == " " and attribute[0][1] != " ")):
+            violations["invalid_attributes_separator"]["value"].append(index)
             break
 
         if attribute[0].rstrip() == "gene_id":
             found_gene_id = True
         elif attribute[0].rstrip() == "transcript_id":
             found_transcript_id = True
+            if record[2] == "inter" and attribute[1] != "":
+                violations["inter_trascript_id_not_empty"]["value"].append(index)
+            elif record[2] == "inter_CNS" and attribute[1] != "":
+                violations["inter_CNS_trascript_id_not_empty"]["value"].append(index)
+            elif record[2] == "inter_CNS" and attribute[1] != "":
+                violations["intron_CNS_trascript_id_empty"]["value"].append(index)
         elif not found_gene_id or not found_transcript_id:
-            violations["invalid_attributes_order"].append(index)
+            violations["invalid_attributes_order"]["value"].append(index)
             break
 
         if re.search(r"\"[\w]+\"|[0-9]+", attribute[1]) == None:
-            violations["text_attribute_not_doublequotes"].append(index)
+            violations["text_attribute_not_doublequotes"]["value"].append(index)
     
     if not found_gene_id:
-        violations["missing_gene_id"].append(index)
+        violations["missing_gene_id"]["value"].append(index)
 
     if not found_transcript_id:
-        violations["missing_transcript_id"].append(index)
+        violations["missing_transcript_id"]["value"].append(index)
+
+# start_codon and stop_codon are not necessarily atomic, but their total lenght must be less than or equal to 3
+if start_codon_bp > 3:
+    violations["start_codon_more_than_3"]["value"] = True
+if stop_codon_bp > 3:
+    violations["stop_codon_more_than_3"]["value"] = True
+
+write_report()
---
+++ violations_ds.py
@@ -0,0 +1,99 @@
+# - Dictionary used to store detected errors
+# - The key represents the error_code. Both "value" and "msg" are stored in the same data structure to avoid keys redundancy
+# - "value" is a boolean whether it refers to a global inter-record error, 
+#   whereas it's a list if it holds line numbers of error occurrences
+# - "msg" is the message that will be printed in the report
+violations = {
+    "source_not_unique": {
+        "value": False,
+        "msg": "\"source\" field must be unique"
+    },
+    "missing_cds": {
+        "value": True,
+        "msg": "A \"CDS\" record is required"
+    },
+    "missing_start_codon": {
+        "value": True,
+        "msg": "A \"start_codon\" record is required"
+    },
+    "missing_stop_codon": {
+        "value": True,
+        "msg": "A \"stop_codon\" record is required"
+    },
+    "start_codon_more_than_3": {
+        "value": False,
+        "msg": "\"start_codon\" total lenght must be less than or equal to 3"
+    },
+    "stop_codon_more_than_3": {
+        "value": False,
+        "msg": "\"stop_codon\" total lenght must be less than or equal to 3"
+    },
+    "invalid_fields_number": {
+        "value": [],
+        "msg": "invalid number fields in the record"
+    },
+    "start_codon_invalid_frame": {
+        "value": [],
+        "msg": "\"frame\" value not valid. \"start_codon\" record allowed values for \"frame\": {0, 1, 2}"
+    },
+    "stop_codon_invalid_frame": {
+        "value": [],
+        "msg": "\"frame\" value not valid. \"stop_codon\" record allowed values for \"frame\": {0, 1, 2}"
+    },
+    "inter_trascript_id_not_empty": {
+        "value": [],
+        "msg": "\"trascript_id\" attribute must be empty for a \"inter\" record"
+    },
+    "inter_CNS_trascript_id_not_empty": {
+        "value": [],
+        "msg": "\"trascript_id\" attribute must be empty for a \"inter_CNS\" record"
+    },
+    "intron_CNS_trascript_id_empty": {
+        "value": [],
+        "msg": "\"trascript_id\" attribute must be non-empty for a \"intron_CNS\" record"
+    },
+    "start_not_valid": {
+        "value": [],
+        "msg": "\"start\" must be an integer greater than or equal to 1"
+    },
+    "end_not_valid": {
+        "value": [],
+        "msg": "\"end\" must be an integer greater than or equal to 1"
+    },
+    "start_greater_than_end": {
+        "value": [],
+        "msg": "\"start\" must be less than or equal to \"end\""
+    },
+    "score_invalid": {
+        "value": [],
+        "msg": "\"score\" must be a numeric value. Dot allowed to"
+    },
+    "strand_invalid": {
+        "value": [],
+        "msg": "\"strand\" value not valid. Allowed values: {+, -}"
+    },
+    "frame_invalid": {
+        "value": [],
+        "msg": "\"frame\" value not valid. Allowed values: {0, 1, 2, .}"
+    },
+    "missing_gene_id": {
+        "value": [],
+        "msg": "\"gene_id\" attribute is required"
+    },
+    "missing_transcript_id": {
+        "value": [],
+        "msg": "\"transcript_id\" attribute is required"
+    },
+    "invalid_attributes_order": {
+        "value": [],
+        "msg": "Invalid attributes order. \"gene_id\" and \"transcript_id\" must precede all the others"
+    },
+    "invalid_attributes_separator": {
+        "value": [],
+        "msg": "Every attribute must end in a semicolon and be separated from the next one by exactly one space"
+    },
+    "text_attribute_not_doublequotes": {
+        "value": [],
+        "msg": "Text attributes must be wrapped by double quotes"
+    },
+}
